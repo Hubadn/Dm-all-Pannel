27dafb5a3962a33013e0b7cea30a1d16e889f68e
Adn <nimouhq@gmail.com>
2023-08-19 12:50:07 +0200

    fix

--- asset_bot/BotPannel.py
+++ asset_bot/BotPannel.py
@@ -1,7 +1,7 @@
 import discord , os
 from discord.ext import commands
 from discord import Webhook, RequestsWebhookAdapter
-
+user_dm = 0
 class SetupTools :
 
 
@@ -20,3 +20,50 @@ client = commands.Bot(command_prefix="pannel!",help_command= None,intents=intent
 async def on_ready():
 
     print("lets config you dm-all")
+
+    guild_id : int = input("enter discord guild id : ")
+    time_sleep : int = input("coldown between send message (if you time sleep is littel your bot can be flagged) : ")
+    type_message = input("what time of message do you want to send\n embed[1]\nmessage[2]")
+    if type_message == "1":
+
+        embed_title = input("title of embed : ")
+        embed_description = input("description of embed : ")
+
+        embed = discord.Embed(
+            title = embed_title,
+            description = embed_description
+        )
+
+        embed_image = input("enter image link (if you don't want press enter)")
+
+        if embed_image == "":
+            pass
+        if embed_image != "":
+            embed.set_image(url= embed_image)
+        
+    guild= client.get_guild(guild_id)
+    print("you dm-all is ready to work ")
+    input(
+        f"""
+guild name :  {guild.name}
+guild members : {len(guild.members)}
+type message : {type_message}
+time sleep  : {time_sleep}
+
+press enter for star dm-all        """)    
+        
+    print(f"user dm-all {user_dm}/{len(guild.members)}")
+    for member in guild.members :
+        if type_message =="1":
+            try :
+                await member.send(embed = embed)
+                user_dm = user_dm  + 1
+            except: 
+                pass
+        
+
+
+
+
+
+            
